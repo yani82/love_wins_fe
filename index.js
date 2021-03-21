e58010e37c1c494c3cd04c0d33c6d026db731a57
yani82 <yani.iman1@gmail.com>
2021-03-20 23:35:40 -0400

    in the post fetch

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const endPoint = "http://localhost:3000/api/v1/posts"
 
 document.addEventListener('DOMContentLoaded', () => {
     getPosts()
+
+    const createPostForm = document.querySelector("#create-post-form") 
+    createPostForm.addEventListener("submit", (e) => createFormHandler(e))
+
 })
 
 function getPosts() {
@@ -22,4 +26,17 @@ function getPosts() {
               document.querySelector('#post-container').innerHTML += postMarkup
           })
     })
+}
+
+function createFormHandler(e) {
+  e.preventDefault()
+  const titleInput = document.querySelector("#input-title").value
+  const contentInput = document.querySelector("#input-content").value
+  const imageInput = document.querySelector("#input-url").value
+  const userId = parseInt(document.querySelector("#users").value) 
+  postFetch(titleInput, contentInput, imageInput, userId)
+}
+
+function postFetch(title, content, image_url, user_id) {
+  console.log(title, content, image_url, user_id)
 }
